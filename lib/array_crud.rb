8182f89745fd4f1b8e69509b952d20ec8dd97e24
Jonah Offitzer <joffitzer@gmail.com>
2019-10-13 22:59:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  the_beatles = ["John", "Paul", "George"]
+  the_beatles = ["Wow", "I", "Am"]
   the_beatles << "Ringo"
 end
 
